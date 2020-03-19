e6202682d9556bc8e06f80edb77995e399f805f8
ShunshunL <shunshunlyu@protonmail.com>
2020-03-19 00:13:35 -0700

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -2,3 +2,4 @@ require 'bundler'
 Bundler.require
 
 # Setup a DB connection here
+db = SQLite3::Database.new(':guests:')
--- db/seed.rb
+++ db/seed.rb
@@ -1,1 +1,7 @@
 # Parse the CSV and seed the database here! Run 'ruby db/seed' to execute this code.
+require 'csv'
+
+table = CSV.parse(File.read('./daily_show_guests.csv'), headers: true)
+
+
+puts table.by_col[2]
